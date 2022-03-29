deb651bda7216d18390c85ca0663d97c2bb1fe9d
Kiran <kiran.ram@gmail.com>
2022-03-29 16:00:25 +0530

    Updated tutorial to show mutability vs immutability

--- tutorial.py
+++ tutorial.py
@@ -1,2 +1,16 @@
-print('Hello World!')
-import pandas as pd
+# demonstrating mutability and immutaility
+
+a = 10
+b = 10
+c = a
+
+print('a is b: ', a is b)
+print('a=b: ', a == b)
+print('c is b: ', c is b)
+
+l = [1, 2, 3]
+m = [1, 2, 3]
+
+print('l is m: ', l is m)
+print('l == m: ', l == m)
+

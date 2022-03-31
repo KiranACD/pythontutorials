ed14b3aae7ff12802cdafb44ad3d8c6bdeb7dd77
Kiran <kiran.ram@gmail.com>
2022-03-31 19:10:22 +0530

    updated float_numbers and added decimal_module

---
+++ numbers/decimal_module.py
@@ -0,0 +1,4 @@
+import decimal
+from decimal import Decimal
+
+print('Decimal global context: ', decimal.getcontext())
--- numbers/float_numbers.py
+++ numbers/float_numbers.py
@@ -22,3 +22,25 @@ b = 0.0000002
 
 print('Checking equality of 0.0000001 and 0.0000002 with math.isclose: ', math.isclose(a, b))
 print('Checking equality of 0.0000001 and 0.0000002 with math.isclose and setting an absolute tolerance: ', math.isclose(a, b, rel_tol=1e-9, abs_tol=1e-05))
+
+# Exploring the round function
+print()
+print('#----------------------------------------------------------------------------------------------------#')
+print()
+
+print('round(18.2): ', round(18.2), type(round(18.2))) # This should return an int
+print('round(18.2, 0): ', round(18.2, 0), type(round(18.2, 0)))
+print('round(18.2, -1): ', round(18.2, -1), type(round(18.2, -1)))
+print('round(18.2, -2): ', round(18.2, -2))
+
+# Bankers rounding rounds to the least significant even number in case of a tie
+print('Bankers rounding round(2.5): ', round(2.5))
+print('Bankers rounding round(3.5): ', round(3.5))
+
+# To round away from 0 in case of a tie
+def round_up(x):
+    x = int(x + 0.5*math.copysign(1, x))
+    return x
+
+print('round away from 0 round_up(2.5): ', round_up(2.5))
+print('round away from 0 round_up(-2.5): ', round_up(-2.5))

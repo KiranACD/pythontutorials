ef549a1890a5573df97a8a407198310bbff3f796
Kiran <kiran.ram@gmail.com>
2022-04-30 00:37:35 +0530

    Associative Arrays

--- Associative_Arrays/file1.py
+++ Associative_Arrays/file1.py
@@ -1,2 +1,5 @@
-a = 'graham'
-print(sum(ord(c) for c in a))
+import sys
+print(sys.hash_info.width)
+print(list(map(hash, [1.1, 2.2, 3.3, 4.4])))
+print(list(map(hash, ['hello', 'python', '!'])))
+print(hash((1, 'a', 10.5)))
